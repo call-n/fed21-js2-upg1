e8275e77d3f2f31140fdd8972995d5c41e72f43f
call-n <callenilsson2001@gmail.com>
2022-08-17 11:57:07 +0200

    nav finished

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,25 +1,63 @@
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import {
+	Box,
+	Flex,
+	Text,
+	HStack,
+} from '@chakra-ui/react'
 import { Link, NavLink } from 'react-router-dom'
 
-const Navigation = () => {
-	return (
-		<Navbar bg="dark" variant="dark" expand="md">
-			<Container>
-				<Navbar.Brand as={Link} to="/">Dudå</Navbar.Brand>
+const items = [
+	{ label: 'Movies', href: '/movies' },
+];
 
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/">Home</Nav.Link>
-						<Nav.Link as={NavLink} end to="/books">Books</Nav.Link>
-						<Nav.Link as={NavLink} end to="/authors">Authors</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	)
+function NavItem(props) {
+	const { children, href, large } = props;
+	return (
+		<NavLink to={href}>
+		<HStack
+			spacing="2"
+			paddingX="3"
+			paddingY={large ? '5' : '2.5'}
+			rounded="lg"
+			_hover={{ color: 'sage.base' }}
+			_activeLink={{ bg: 'maincolor.midbg', border: '1px solid', borderColor: 'maincolor.bordergray' }}
+		>
+			<Text fontFamily="heading">{children}</Text>
+		</HStack>
+		</NavLink>
+	);
 }
 
-export default Navigation
+function DesktopNavItemGroup(props) {
+	return (
+		<HStack as="nav" aria-label="Main navigation" spacing="8" {...props}>
+		{items.map((item) => (
+			<NavItem
+			key={item.label}
+			href={item.href}
+			>
+			{item.label}
+			</NavItem>
+		))}
+		</HStack>
+	);
+}
+
+function Headshot() {
+	return (
+		<Flex flexWrap="nowrap" gap="5px" justifyContent="flex-start" alignItems="center" fontSize="25px" fontWeight="600">
+			<Link to="/">
+				Dababy movies
+			</Link>
+		</Flex>
+	);
+}
+
+export default function Navigation() {
+  return (
+    <Box as="header" display="flex" justifyContent="space-between" alignContent="center" minHeight="73px" fontSize="18px" color="rgb(210, 211, 215)" backgroundColor="rgb(32, 33, 36)" padding="0 clamp(1.375rem, 1.2rem + 0.89vw, 2rem)" borderBottom="1px solid" borderColor="rgb(88, 91, 99)" lineHeight="1">
+		<Headshot />
+		<DesktopNavItemGroup />
+    </Box>
+  );
+}
