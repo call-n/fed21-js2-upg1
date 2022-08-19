a9373743cdae266b3cd35aea52413d37bbeaf0ab
call-n <callenilsson2001@gmail.com>
2022-08-19 11:55:32 +0200

    All movies, actor and movie details page

---
+++ src/pages/AllMovies.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+import MovieAPI from '../services/MovieAPI'
+import LoadingSpinner from '../components/LoadingSpinner'
+import MovieCard from '../components/MovieCard'
+import GenrePicker from '../components/GenrePicker'
+import {
+	Box,
+	Heading
+} from '@chakra-ui/react'
+
+const AllMovies = () => {
+    const { data, error, isError, isLoading, isSuccess } = useQuery('all-movies', MovieAPI.getAllLatest)
+    
+  return (
+    <Box>
+        {isLoading && <LoadingSpinner />}
+
+		{isError && error.message}
+
+        <Box paddingBlock="clamp(3.75rem, 3.21rem + 2.68vw, 5.625rem)">
+            <Box 
+                marginInline="auto"
+                width="clamp(16rem, 95vw, 85rem)"
+                paddingInline="clamp(1.375rem, 1.2rem + 0.89vw, 2rem)"
+                position="relative"
+            >
+                <Heading marginBottom="3rem" fontSize="5xl">All Movies</Heading>
+
+                <Heading marginBottom="2rem">Genres</Heading>
+
+                <GenrePicker />
+            </Box>
+        </Box>
+
+        <Box>
+            <Box 
+                marginInline="auto"
+                width="clamp(16rem, 95vw, 85rem)"
+                paddingInline="clamp(1.375rem, 1.2rem + 0.89vw, 2rem)"
+                position="relative"
+                display="grid" 
+                gridTemplateColumns={{ base: 'repeat(auto-fit, minmax(16rem, 1fr))', md: 'repeat(auto-fill, minmax(22rem, 1fr))', }} 
+                gridGap="clamp(1.375rem, 1.2rem + 0.89vw, 2rem)"
+            >
+            {isSuccess && 
+                data.results.map((movie, i) => (
+                    <MovieCard key={i} movie={movie} />
+                ))
+            }
+            </Box>
+        </Box>
+    </Box>
+  )
+}
+
+export default AllMovies
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,184 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+import { useParams, Link } from 'react-router-dom'
+import LoadingSpinner from '../components/LoadingSpinner'
+import MovieAPI from '../services/MovieAPI'
+import {
+	Box,
+	Heading,
+    Flex,
+    Text,
+    Image,
+    Wrap,
+    Stack,
+    Button,
+    Center,
+    Avatar,
+} from '@chakra-ui/react'
+
+const MovieDetails = () => {
+    const { id } = useParams()
+    const { data, error, isError, isLoading, isSuccess } = useQuery(['single-movie', {id}], () => MovieAPI.getMovie(id))
+
+    const timeConvert = (n) => {
+        var num = n
+        var hours = (num / 60)
+        var rhours = Math.floor(hours)
+        var minutes = (hours - rhours) * 60
+        var rminutes = Math.round(minutes)
+        return rhours + "h " + rminutes + "m"
+    }
+
+  return (
+    <Box>
+        {isLoading && <LoadingSpinner />}
+
+		{isError && error.message}
+
+        <Box paddingBlock="clamp(3.75rem, 3.21rem + 2.68vw, 5.625rem)">
+            <Box 
+                marginInline="auto"
+                width="clamp(16rem, 95vw, 85rem)"
+                paddingInline="clamp(1.375rem, 1.2rem + 0.89vw, 2rem)"
+                position="relative"
+            >
+            {isSuccess && (
+                <>
+                    <Flex flexDirection={{lg: 'row', base: 'column'}}>
+                        <Flex overflow="hidden" position="relative" zIndex="1" justifyContent="center">
+                            <Box>
+                                <img 
+                                    src={MovieAPI.movieImagesEndpoint + '/' + data.poster_path}
+                                    style={{borderRadius: '10px', height: 'auto', maxWidth: '100%'}}
+                                />
+                            </Box>
+                        </Flex>
+                        
+                        <Flex justifyContent="center">
+                            <Flex flexDirection="column" maxWidth="40rem" paddingLeft={{lg: "3rem", md: "0rem"}}>
+                                <Heading marginTop="2rem" marginBottom="1rem" fontSize="4xl">{data.original_title} ({data.release_date.substring(0, 4)})</Heading>
+
+                                <Wrap gap="3" fontWeight="600" marginBottom="1rem">
+                                    <Text color="#c58af9">
+                                        {data.release_date} ({data.original_language})
+                                    </Text>
+                                    {data.genres.map((genre, i) => (
+                                        <Link key={i} to={'/genre/' + genre.id}>
+                                            <Text color="#c58af9" _hover={{textDecor: "underline"}}>
+                                                {genre.name},
+                                            </Text>
+                                        </Link>
+                                    ))}
+                                    <Text color="#c58af9" _hover={{textDecor: "underline"}}>
+                                        {timeConvert(data.runtime)}
+                                    </Text>
+                                </Wrap>
+
+                                <Text fontSize="18px" color="gray.300">
+                                    {data.tagline}
+                                </Text>
+
+                                <Heading marginTop="2rem" marginBottom="1rem" fontSize="2xl">
+                                    Overview
+                                </Heading>
+
+                                <Text fontSize="18px">
+                                    {data.overview}
+                                </Text>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                    <Heading marginTop="2rem" fontSize="4xl">
+                    Cast
+                    </Heading>
+
+                    <Flex overflowY="hidden" overflowX="scroll">
+                        {data.credits.cast.filter(actor => actor.order < 8).map((actor, i) => (
+                            <ActorProfile key={i} props={actor} />
+                        ))}
+                    </Flex>
+                </>
+            )}
+            </Box>
+        </Box>
+
+    </Box>
+  )
+}
+
+function ActorProfile({props}) {
+	const { name, character, id, profile_path, popularity } = props;
+	return (
+        <Center 
+            py={2} 
+        >
+            <Box
+                maxW={'270px'}
+                w={'full'}
+                bg={'gray.800'}
+                boxShadow={'2xl'}
+                rounded={'md'}
+                margin="0.5rem"
+                border="1px solid #585b63"
+                overflow={'hidden'}>
+                <Image
+                h={'120px'}
+                w={'full'}
+                src={MovieAPI.movieImagesEndpoint + '/' + profile_path}
+                objectFit={'cover'}
+                />
+                <Flex justify={'center'} mt={-12}>
+                <Avatar
+                    size={'xl'}
+                    src={MovieAPI.movieImagesEndpoint + '/' + profile_path}
+                    alt={'Actor'}
+                    css={{
+                    border: '2px solid white',
+                    }}
+                />
+                </Flex>
+
+                <Flex p={4} flexDirection="column" minWidth="260px">
+                    <Stack spacing={0} align={'center'} mb={5}>
+                        <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+                        {name}
+                        </Heading>
+                        <Text color={'gray.500'}>{character}</Text>
+                    </Stack>
+
+                    <Stack direction={'row'} justify={'center'} spacing={6}>
+                        <Stack spacing={0} align={'center'}>
+                        <Text fontWeight={600}>{id}</Text>
+                        <Text fontSize={'sm'} color={'gray.500'}>
+                            id
+                        </Text>
+                        </Stack>
+                        <Stack spacing={0} align={'center'}>
+                        <Text fontWeight={600}>{popularity}</Text>
+                        <Text fontSize={'sm'} color={'gray.500'}>
+                            popularity
+                        </Text>
+                        </Stack>
+                    </Stack>
+
+                    <Link to={'/actor/' + id}>
+                        <Button
+                            w={'full'}
+                            mt={8}
+                            bg={'gray.900'}
+                            color={'white'}
+                            rounded={'md'}
+                            _hover={{
+                            transform: 'translateY(-2px)',
+                            boxShadow: 'lg',
+                            }}>
+                            Read more
+                        </Button>
+                    </Link>
+                </Flex>
+            </Box>
+            </Center>
+	);
+}
+
+export default MovieDetails
